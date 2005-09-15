93acee49f4e128d7e987bde58d4d4fee1557a6e8
vecego <vecego>
2005-09-15 19:48:56 +0000

    Set Session to null

--- Springbreak/src/at/newsagg/web/IndexViewController.java
+++ Springbreak/src/at/newsagg/web/IndexViewController.java
@@ -2,11 +2,13 @@ package at.newsagg.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import at.newsagg.model.User;
 
 
@@ -15,7 +17,9 @@ public class IndexViewController implements Controller {
 	private static Log log = LogFactory.getLog(IndexViewController.class); 
 	
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception { 
-		User user = new User();
-		return new ModelAndView("index", "login", user); 
+		
+	    HttpSession session = request.getSession();
+	    session = null;
+		return new ModelAndView("index", "login", new User()); 
 	}
 }
