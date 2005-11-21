86728d16c72cc6db768eff1ddda4aad274761808
szabolcs <szabolcs>
2005-11-21 16:15:18 +0000

    kleinigkeiten in der suche

--- Springbreak/src/at/newsagg/search/RssDbIndexer.java
+++ Springbreak/src/at/newsagg/search/RssDbIndexer.java
@@ -1,22 +1,25 @@
 package at.newsagg.search;
 
-import java.util.Vector;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.io.IOException;
-import java.io.File;
-import java.util.ArrayList;
 
-import org.apache.lucene.document.*;
-import org.apache.lucene.index.*;
-import org.apache.lucene.analysis.standard.StandardAnalyzer; 
-import org.apache.lucene.search.Query;
-import org.apache.lucene.queryParser.QueryParser;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.Hits;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 
-import at.newsagg.search.RssDbIndexer;
 import at.newsagg.dao.ItemDAO;
 import at.newsagg.model.parser.hibernate.Item;
 
@@ -27,6 +30,7 @@ import at.newsagg.model.parser.hibernate.Item;
  *
  */
 public class RssDbIndexer {
+	private static Log log = LogFactory.getLog(RssDbIndexer.class);
     private ItemDAO itemDao;
     private String indexLocation;
     private File index;
@@ -52,10 +56,10 @@ public class RssDbIndexer {
                         doc.add(Field.Text("id",  Long.toString(rssItem.getId())));
                         doc.add(Field.Text("title", rssItem.getTitle()));
                         doc.add(Field.Text("description", rssItem.getTitle()));
-                                writer.addDocument(doc);
-                                indexCreated = true;
+                        writer.addDocument(doc);
+                        indexCreated = true;
                     }
-                    
+                    log.debug("#### docCount=" + writer.docCount());
                     writer.optimize();
                     writer.close();
                 }
@@ -72,34 +76,52 @@ public class RssDbIndexer {
      * @param numberOfResults 
      */
     public void search(String search, int numberOfResults) {
-            System.out.println("#### search " + search);
+    	log.debug("#### searchQuery=" + search);
+    	log.debug("### numberOfResults " + numberOfResults);
+    	try {
+	    	Directory fsDir = FSDirectory.getDirectory(index, false);
+	    	IndexSearcher is = new IndexSearcher(fsDir);
+	    	
+	    	Query query = QueryParser.parse(search, "description", new StandardAnalyzer());
+	    	
+	    	long start = new Date().getTime();
+	    	Hits hits = is.search(query);
+	    	long end = new Date().getTime();
+	    	
+	    	for (int i = 0; i < hits.length(); i++) {
+	    		Document doc = hits.doc(i);
+	    		log.debug("#### found object id = " + doc.get("id"));
+	    		log.debug("#### found object id = " + doc.get("id"));
+	    	}
+    	} catch (IOException e) {
+    		 e.printStackTrace();
+    	} catch (ParseException e) {
+    		 e.printStackTrace();
+    	}
+    	
+ /*           log.debug("#### searchQuery=" + search);
             System.out.println("#### numberOfResults " + numberOfResults);
             // create query from searchstring
             Query query = null;
             try {
                 query = QueryParser.parse(search, "description", new StandardAnalyzer());
-                System.out.println("#### 1");
             }
             catch (ParseException e) {
                 e.printStackTrace();
             }
-            System.out.println("#### 2");
             // create List with ids to retrieve the rss db objects
             ArrayList ids = new ArrayList();
             try {
-                System.out.println("#### 3");
                 IndexReader reader = IndexReader.open(index);
                 IndexSearcher searcher = new IndexSearcher(reader);
                 Hits hits = searcher.search(query);
-                System.out.println("#### 4");
                 for (int i = 0; i != hits.length() && i != numberOfResults; ++i) {
-                    System.out.println("#### 5");
                     Document doc = hits.doc(i);
                     ids.add(new Integer(doc.getField("id").stringValue()));
                 }
                 searcher.close();
                 reader.close();
-                System.out.println("#### 6 size " + ids.size());
+                log.debug("#### id Object size = " + ids.size());
                 for (Iterator iter = ids.iterator(); iter.hasNext(); ) { 
                     Integer id = (Integer) iter.next();
                     System.out.println("#### " + id);
@@ -108,11 +130,9 @@ public class RssDbIndexer {
             catch (IOException e) {
                 e.printStackTrace();
             }
-            
+            */
             
             // create rss objects based on the resultset
-            
-        
     }
 
     public ItemDAO getItemDao() {
