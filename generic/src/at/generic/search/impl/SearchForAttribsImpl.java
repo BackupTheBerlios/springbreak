cdfcfe827fe1a66df0031698f63a1a69f8714582
szabolcs <szabolcs>
2006-02-12 16:35:50 +0000

    String Attributzerleger

--- generic/src/at/generic/search/impl/SearchForAttribsImpl.java
+++ generic/src/at/generic/search/impl/SearchForAttribsImpl.java
@@ -17,9 +17,9 @@ import at.generic.service.EventHandling;
 
 /**
  * @author szabolcs
- * @version $Id: SearchForAttribsImpl.java,v 1.1 2006/02/12 16:31:14 szabolcs Exp $
+ * @version $Id: SearchForAttribsImpl.java,v 1.2 2006/02/12 16:35:50 szabolcs Exp $
  * $Author: szabolcs $  
- * $Revision: 1.1 $
+ * $Revision: 1.2 $
  * 
  * Searchinterface for Events.
  * 
@@ -93,19 +93,25 @@ public class SearchForAttribsImpl implements SearchForAttribs {
 		return resultList;
 	}
 	
+	/**
+	 * Creates a list with search attributes. Treats a double quoted string as one attribute 
+	 * 
+	 * @param searchStr
+	 * @return Vector with search attributes
+	 */
 	private Vector createSearchAttributeList(String searchStr) {
 		Vector searchAttribs = new Vector();
 
 		searchStr.trim();
 		
-		log.debug("### searchStr.length(): " + searchStr.length());
+		//log.debug("### searchStr.length(): " + searchStr.length());
 		
 		boolean openQuote = false;
 		int i = 0;
 		int j = 0;
 		
 		for (j = 0; j < searchStr.length(); j++) {
-			log.debug("### i=" + i + "; j=" + j);
+			//log.debug("### i=" + i + "; j=" + j);
 			if (((searchStr.substring(j,j+1).equals(" ") || (j == (searchStr.length() - 1))) && openQuote == false) ) {
 				log.debug("### substring (i=" + i + ";j+1=" + j+1 + "): #" + searchStr.substring(i, j + 1).trim() + "#");
 				searchAttribs.add(searchStr.substring(i, j + 1).trim());
@@ -121,65 +127,8 @@ public class SearchForAttribsImpl implements SearchForAttribs {
 			}
 			
 		}
-		
-		
-		/*String concStr = new String();
-		boolean openQuote = false;
-		int i = 0;
-		int j = 0;
-		
-		for (j = 0; j < searchStr.length(); j++) {
-			//log.debug("### searchStr.substring(j,j+1): " + searchStr.substring(j,j+1));
-			if (searchStr.substring(j,j+1).equals("\"")) {
-				//log.debug("### entered i=" + i + "; j = " + j);
-				if (openQuote == false) {
-					openQuote = true;
-					concStr = concStr + searchStr.substring(i, j);
-					i = j + 1;
-				} else {
-					openQuote = false;
-					//log.debug("### substring: #" + searchStr.substring(i, j).trim() + "#");
-					searchAttribs.add(searchStr.substring(i, j).trim());
-					i = j + 1;
-				}
-			}
-		}
-		//log.debug("### concStr: " + concStr);
-		
-		StringTokenizer st = new StringTokenizer(concStr);
-		while (st.hasMoreTokens()) {
-			//log.debug("### search token: " + st.nextToken());
-			searchAttribs.add(st.nextToken());
-		}
-		
-		Iterator iter = searchAttribs.iterator();
-		while (iter.hasNext()) {
-			String ea = (String) iter.next();
-			log.debug("### Search Attrib List: " + ea);
-		}*/
-	     
-	     return searchAttribs;
-	}
 	
-	private void createSearchAttributeListaux(Vector searchAttribs, String searchStr, int i, int j) {
-		/*i = searchStr.indexOf("\"", j);
-		log.debug("### i: " + i);
-		j = searchStr.indexOf("\"", i + 1);
-		log.debug("### j: " + j);
-		
-		if (i != -1 && j != -1) {
-			log.debug("### searchStr.substring(i, j-1): " + searchStr.substring(i+1, j));
-			searchAttribs.add(searchStr.substring(i+1, j));
-			createSearchAttributeListaux(searchAttribs, searchStr, 0, j);
-		}
-		i = 0;
-		for (j = 0; j < searchStr.length(); j++) {
-			log.debug("### searchStr.substring(j,j+1): " + searchStr.substring(j,j+1));
-			if (searchStr.substring(j,j+1).equals(" ")) {
-				log.debug("### substring: #" + searchStr.substring(i, j) + "#");
-				i = j + 1;
-			}
-		}*/
+	     return searchAttribs;
 	}
 	
 	/**
