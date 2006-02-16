441b676618612fabfc4f8a767777ed6763e89641
szabolcs <szabolcs>
2006-02-16 17:23:25 +0000

    *** empty log message ***

---
+++ sena/SzabolcsDiplomarbeit/Diplomarbeit Struktur.mm
@@ -0,0 +1,41 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1140098029562" ID="Freemind_Link_7714901" MODIFIED="1140098570984" TEXT="Diplomarbeit Struktur">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1140098125093" ID="Freemind_Link_847796441" MODIFIED="1140098573046" POSITION="right" TEXT="Einf&#xfc;hrung">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1140098055718" ID="_" MODIFIED="1140098062578" TEXT="Bedeutung von Events"/>
+<node CREATED="1140098111750" ID="Freemind_Link_1957745656" MODIFIED="1140098140796" TEXT="Bedeutung der Auffindung von Daten">
+<node CREATED="1140098336718" ID="Freemind_Link_1345148021" MODIFIED="1140098366796" TEXT="Probleme"/>
+<node CREATED="1140098369296" ID="Freemind_Link_1843524986" MODIFIED="1140098399343" TEXT="L&#xf6;sungsans&#xe4;tze">
+<node CREATED="1140098400515" ID="Freemind_Link_1634826686" MODIFIED="1140098401625" TEXT="Google"/>
+</node>
+<node CREATED="1140098406796" ID="Freemind_Link_633003820" MODIFIED="1140098420859" TEXT="Konkrete Technologien/Architekturen"/>
+</node>
+<node CREATED="1140098147312" ID="Freemind_Link_509936434" MODIFIED="1140098241843" TEXT="aktuelle Forschung/Arbeiten">
+<node CREATED="1140098083890" ID="Freemind_Link_748765300" MODIFIED="1140098094406" TEXT="Vorstellung des Senactive Konzeptes"/>
+<node CREATED="1140098248109" ID="Freemind_Link_513671466" MODIFIED="1140098257562" TEXT="Das Zeug was die in Stanford machen"/>
+<node CREATED="1140098425562" ID="Freemind_Link_1944109159" MODIFIED="1140098432000" TEXT="... &lt;recherchieren&gt;"/>
+</node>
+<node CREATED="1140098155046" ID="Freemind_Link_841435694" MODIFIED="1140098161281" TEXT="Anwendungsgebiete"/>
+</node>
+<node CREATED="1140098221734" ID="Freemind_Link_978760588" MODIFIED="1140098575265" POSITION="left" TEXT="Event Google">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1140098226843" ID="Freemind_Link_1662021094" MODIFIED="1140098281250" TEXT="Wozu braucht man ein Event Google?"/>
+<node CREATED="1140098200265" ID="Freemind_Link_1816163245" MODIFIED="1140098464515" TEXT="Beschreibung der Suchkonzepte Rang 1,2,3"/>
+<node CREATED="1140098471703" ID="Freemind_Link_723079705" MODIFIED="1140098480109" TEXT="Probleme die es zu l&#xf6;sen gibt">
+<node CREATED="1140098300234" ID="Freemind_Link_1812908211" MODIFIED="1140098305593" TEXT="Systemarchitektur"/>
+</node>
+<node CREATED="1140098074140" ID="Freemind_Link_258854900" MODIFIED="1140098107734" TEXT="Generierung der Beispieldaten - Simulation"/>
+<node CREATED="1140098490234" ID="Freemind_Link_1859116325" MODIFIED="1140098495328" TEXT="Evaluierung der Performance"/>
+</node>
+<node CREATED="1140098502671" ID="Freemind_Link_1819220227" MODIFIED="1140098577671" POSITION="left" TEXT="Folgerungen">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1140098513312" ID="Freemind_Link_354872168" MODIFIED="1140098532828" TEXT="offene Fragestellungen"/>
+<node CREATED="1140098522671" ID="Freemind_Link_949980809" MODIFIED="1140098553828" TEXT="Was bringt mir Event Google unterm Strich?"/>
+</node>
+</node>
+</map>
