fefa567f3d914298ca85ecd18fa90ac608618b56
szabolcs <szabolcs>
2006-03-25 14:22:19 +0000

    Verfeinerungen in der Einleitung

--- sena/SzabolcsDiplomarbeit/Diplomarbeit Struktur.mm
+++ sena/SzabolcsDiplomarbeit/Diplomarbeit Struktur.mm
@@ -2,11 +2,191 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1140098029562" ID="Freemind_Link_7714901" MODIFIED="1140098570984" TEXT="Diplomarbeit Struktur">
 <font BOLD="true" NAME="SansSerif" SIZE="14"/>
-<node CREATED="1140098125093" ID="Freemind_Link_847796441" MODIFIED="1140098573046" POSITION="right" TEXT="Einf&#xfc;hrung">
+<node CREATED="1140098125093" ID="Freemind_Link_847796441" MODIFIED="1141929004495" POSITION="right" TEXT="Theorie/Konzept">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="full-1"/>
-<node CREATED="1140098055718" ID="_" MODIFIED="1140098062578" TEXT="Bedeutung von Events">
+<node CREATED="1143038835734" ID="Freemind_Link_1569510968" MODIFIED="1143038847370" TEXT="Introduction">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1143038848622" ID="Freemind_Link_1698741348" MODIFIED="1143209330100" TEXT="Current IT landscape">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1143038889090" ID="Freemind_Link_612671510" MODIFIED="1143209328228" TEXT="distributed and connected through Internet"/>
+<node CREATED="1143038924551" ID="Freemind_Link_1724365718" MODIFIED="1143209326125" TEXT="automated workflow">
+<node CREATED="1143038936058" ID="Freemind_Link_1378640320" MODIFIED="1143038944410" TEXT="banking and financial transactions"/>
+<node CREATED="1143038949047" ID="Freemind_Link_246979355" MODIFIED="1143038957529" TEXT="warehousing systems"/>
+<node CREATED="1143038959942" ID="Freemind_Link_548012281" MODIFIED="1143038961565" TEXT="factories"/>
+</node>
+<node CREATED="1143040281956" ID="Freemind_Link_458058028" MODIFIED="1143209324592" TEXT="electronic collaboration&#xa;(e-commerce)">
+<node CREATED="1143038992679" ID="Freemind_Link_357259842" MODIFIED="1143038997907" TEXT="trading partnerships"/>
+<node CREATED="1143038967994" ID="Freemind_Link_1052313660" MODIFIED="1143038982935" TEXT="Information is shared between enterprises"/>
+<node CREATED="1143039010185" ID="Freemind_Link_543759374" MODIFIED="1143039021982" TEXT="each of them has got an own internal information processing system"/>
+<node CREATED="1143040307653" ID="Freemind_Link_1411541680" MODIFIED="1143040313512" TEXT="advantage">
+<node CREATED="1143040314363" ID="Freemind_Link_1770731260" MODIFIED="1143040318288" TEXT="reduce costs"/>
+<node CREATED="1143040319480" ID="Freemind_Link_1424400441" MODIFIED="1143040326220" TEXT="lower inventory levels">
+<node CREATED="1143040359628" ID="Freemind_Link_1513289762" MODIFIED="1143040365116" TEXT="justIntime"/>
+</node>
+<node CREATED="1143040327592" ID="Freemind_Link_33942707" MODIFIED="1143040331497" TEXT="faster time2market"/>
+<node CREATED="1143040336244" ID="Freemind_Link_130071677" MODIFIED="1143040342403" TEXT="increase profitability"/>
+<node CREATED="1143040343635" ID="Freemind_Link_1266110426" MODIFIED="1143040355332" TEXT="increase competitiveness"/>
+</node>
+<node CREATED="1143040371735" ID="Freemind_Link_1196373185" MODIFIED="1143040379476" TEXT="trading hubs">
+<node CREATED="1143040396962" ID="Freemind_Link_1324230441" MODIFIED="1143040407216" TEXT="standards beween enterprises"/>
+<node CREATED="1143040411593" ID="Freemind_Link_1200491835" MODIFIED="1143040414467" TEXT="XML,...."/>
+</node>
+</node>
+<node CREATED="1143039041079" ID="Freemind_Link_156358598" MODIFIED="1143039064112" TEXT="Example Government and military information systems next to commerical"/>
+</node>
+<node CREATED="1143039302715" ID="Freemind_Link_1802423830" MODIFIED="1143209319135" TEXT="Problems">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1143038867219" ID="Freemind_Link_303854197" MODIFIED="1143039268626" TEXT="Problem of monitoring and managing information">
+<node CREATED="1143039100284" ID="Freemind_Link_365347976" MODIFIED="1143039116798" TEXT="billions of events running throug those systems"/>
+<node CREATED="1143039117960" ID="Freemind_Link_1079034884" MODIFIED="1143039136126" TEXT="no technology that helps to view those events in ways we humans understand"/>
+<node CREATED="1143039143146" ID="Freemind_Link_60460880" MODIFIED="1143039151157" TEXT="we can see the events but making sense is a problem"/>
+<node CREATED="1143039157667" ID="Freemind_Link_1953599697" MODIFIED="1143039178326" TEXT="we already have got network monitors to say that the router in Hong Kong is making trouble"/>
+<node CREATED="1143039204153" ID="Freemind_Link_1365588426" MODIFIED="1143039232785" TEXT="we need ways to look a complex high level problems to answear strategic and business problems"/>
+</node>
+<node CREATED="1143039251221" ID="Freemind_Link_989056025" MODIFIED="1143039262908" TEXT="Problem of tracking causes of events"/>
+<node CREATED="1143039311688" ID="Freemind_Link_1233028848" MODIFIED="1143039346308" TEXT="Don&apos;t understand communication traffic">
+<node CREATED="1143039347500" ID="Freemind_Link_1071978567" MODIFIED="1143039351996" TEXT="Who is talking to Whom"/>
+<node CREATED="1143039353158" ID="Freemind_Link_1023675647" MODIFIED="1143039376011" TEXT="very dynamic communication architecture"/>
+<node CREATED="1143039385054" ID="Freemind_Link_297373099" MODIFIED="1143039393015" TEXT="most of the time we don&apos;t know what is happening"/>
+</node>
+</node>
+<node CREATED="1143040445321" ID="Freemind_Link_1823741269" MODIFIED="1143040580896" TEXT="Goals/Solutions">
+<node CREATED="1143040530554" ID="Freemind_Link_1585215749" MODIFIED="1143040534109" TEXT="Situations of interest">
+<node CREATED="1143040474834" ID="Freemind_Link_637638532" MODIFIED="1143040481002" TEXT="Risk mitigation"/>
+<node CREATED="1143040482074" ID="Freemind_Link_804860694" MODIFIED="1143040487522" TEXT="Policy enforcement"/>
+<node CREATED="1143040488583" ID="Freemind_Link_1369635376" MODIFIED="1143040497496" TEXT="Opportunistic trading"/>
+<node CREATED="1143040498648" ID="Freemind_Link_1141920659" MODIFIED="1143040506820" TEXT="Regulatory monitoring"/>
+</node>
+<node CREATED="1143040541950" ID="Freemind_Link_118262914" MODIFIED="1143040551614" TEXT="Real-time marketplace decisions"/>
+<node CREATED="1143040723962" ID="Freemind_Link_354252985" MODIFIED="1143040733796" TEXT="Monitor events at every level in IT systems"/>
+<node CREATED="1143040553296" ID="Freemind_Link_1089808402" MODIFIED="1143040564182" TEXT="track causal relationships">
+<node CREATED="1143040564963" ID="Freemind_Link_57138031" MODIFIED="1143040568288" TEXT="horizontal"/>
+<node CREATED="1143040569420" ID="Freemind_Link_1189237927" MODIFIED="1143040570862" TEXT="vertical"/>
+</node>
+<node CREATED="1143040597961" ID="Freemind_Link_734292612" MODIFIED="1143040604490" TEXT="Scaleability to High level"/>
+<node CREATED="1143040748377" ID="Freemind_Link_1210890345" MODIFIED="1143040751862" TEXT="Pattern recognition">
+<node CREATED="1143040757740" ID="Freemind_Link_1628898076" MODIFIED="1143040765532" TEXT="react on recognised Patterns"/>
+<node CREATED="1143040926573" ID="Freemind_Link_1090232895" MODIFIED="1143040940854" TEXT="We cannot react on every situation or define rules for everything">
+<node CREATED="1143040956516" ID="Freemind_Link_235291585" MODIFIED="1143040972179" TEXT="be able to use Event system to gather intelligence"/>
+<node CREATED="1143040978888" ID="Freemind_Link_737177060" MODIFIED="1143040982293" TEXT="understand situations"/>
+<node CREATED="1143040986980" ID="Freemind_Link_1746926083" MODIFIED="1143041012707" TEXT="find causes for problems"/>
+<node CREATED="1143041097739" ID="Freemind_Link_117991703" MODIFIED="1143041107463" TEXT="problem of delivering relevant information">
+<node CREATED="1143041116817" ID="Freemind_Link_1586226343" MODIFIED="1143041120622" TEXT="information gap">
+<node CREATED="1143041121563" ID="Freemind_Link_1895056167" MODIFIED="1143041125770" TEXT="vertical dimension"/>
+<node CREATED="1143041126991" ID="Freemind_Link_175921241" MODIFIED="1143041131307" TEXT="horizontal dimension"/>
+</node>
+<node CREATED="1143041148682" ID="Freemind_Link_1197889625" MODIFIED="1143041153840" TEXT="problem relevant information"/>
+<node CREATED="1143041140581" ID="Freemind_Link_1903221337" MODIFIED="1143041142784" TEXT="profiles"/>
+<node CREATED="1143041042189" ID="Freemind_Link_1960364504" MODIFIED="1143041047377" TEXT="aggreagate events"/>
+<node CREATED="1143041037503" ID="Freemind_Link_1115892396" MODIFIED="1143041041008" TEXT="&quot;drill-down&quot;"/>
+<node CREATED="1143041206175" ID="Freemind_Link_705157938" MODIFIED="1143041211242" TEXT="Immediate Interest">
+<node CREATED="1143041213866" ID="Freemind_Link_1720610896" MODIFIED="1143041215599" TEXT="Relevance"/>
+<node CREATED="1143041216640" ID="Freemind_Link_412292086" MODIFIED="1143041221397" TEXT="Understandability"/>
+<node CREATED="1143041222739" ID="Freemind_Link_700871997" MODIFIED="1143041228067" TEXT="Ease of analysis"/>
+<node CREATED="1143041229569" ID="Freemind_Link_1686333227" MODIFIED="1143041239143" TEXT="causality, timing and aggregation"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1143040887287" ID="Freemind_Link_213723849" MODIFIED="1143040904972" TEXT="need to match complex event patterns int the global event cloud"/>
+</node>
+</node>
+<node CREATED="1140098055718" ID="_" MODIFIED="1141928833219" TEXT="Bedeutung von Events">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1143036394083" ID="Freemind_Link_978903069" MODIFIED="1143036396757" TEXT="Events">
+<node CREATED="1143036397858" ID="Freemind_Link_1397872395" MODIFIED="1143036401854" TEXT="Definitions">
+<node CREATED="1143036596995" ID="Freemind_Link_552532721" MODIFIED="1143036611666" TEXT="Difference between message and event"/>
+<node CREATED="1143036469331" ID="Freemind_Link_1857557131" MODIFIED="1143036473126" TEXT="Three aspects">
+<node CREATED="1143036473918" ID="Freemind_Link_945184578" MODIFIED="1143036476061" TEXT="Form"/>
+<node CREATED="1143036477242" ID="Freemind_Link_1290059162" MODIFIED="1143036481999" TEXT="Siginificance"/>
+<node CREATED="1143036484202" ID="Freemind_Link_1298151960" MODIFIED="1143036487808" TEXT="Relativity"/>
+</node>
+<node CREATED="1143036494387" ID="Freemind_Link_315739865" MODIFIED="1143036496690" TEXT="Example"/>
+<node CREATED="1143036533143" ID="Freemind_Link_1906304968" MODIFIED="1143036539692" TEXT="How they are created">
+<node CREATED="1143036541355" ID="Freemind_Link_1721972029" MODIFIED="1143036546893" TEXT="Observation"/>
+<node CREATED="1143036548014" ID="Freemind_Link_77678205" MODIFIED="1143036553893" TEXT="Adaptation"/>
+</node>
+<node CREATED="1143040799280" ID="Freemind_Link_586648844" MODIFIED="1143040807141" TEXT="What means Event-Driven?"/>
+<node CREATED="1143040819699" ID="Freemind_Link_22592725" MODIFIED="1143040826169" TEXT="Event Cloud!">
+<node CREATED="1143040853678" ID="Freemind_Link_65012084" MODIFIED="1143040855972" TEXT="nonlinear"/>
+<node CREATED="1143040857454" ID="Freemind_Link_1023855051" MODIFIED="1143040863332" TEXT="event triggers"/>
+</node>
+</node>
+<node CREATED="1143036417286" ID="Freemind_Link_1848319200" MODIFIED="1143036430826" TEXT="Where do they come from?">
+<node CREATED="1143036290804" ID="Freemind_Link_188535374" MODIFIED="1143036310643" TEXT="Messages">
+<node CREATED="1143036338102" ID="Freemind_Link_892158244" MODIFIED="1143036343971" TEXT="Messaging Types">
+<node CREATED="1143036647998" ID="Freemind_Link_1064657859" MODIFIED="1143036656550" TEXT="publish/subscribe"/>
+<node CREATED="1143036657562" ID="Freemind_Link_1161066856" MODIFIED="1143036665623" TEXT="asynchronouse..."/>
+<node CREATED="1143036666805" ID="Freemind_Link_1036772227" MODIFIED="1143036668878" TEXT="...."/>
+</node>
+<node CREATED="1143036312976" ID="Freemind_Link_210118831" MODIFIED="1143036637873" TEXT="Message oriented Middleware">
+<node CREATED="1143036356489" ID="Freemind_Link_846954227" MODIFIED="1143036358742" TEXT="Middleware">
+<node CREATED="1143036368346" ID="Freemind_Link_1484599405" MODIFIED="1143036372362" TEXT="What for?"/>
+<node CREATED="1143036359914" ID="Freemind_Link_985127280" MODIFIED="1143036361666" TEXT="Definition"/>
+<node CREATED="1143036363399" ID="Freemind_Link_114113237" MODIFIED="1143036365331" TEXT="Products"/>
+</node>
+</node>
+<node CREATED="1143036327477" ID="Freemind_Link_1359302323" MODIFIED="1143036336169" TEXT="Message Brokers">
+<node CREATED="1143036749514" ID="Freemind_Link_1344706478" MODIFIED="1143036754351" TEXT="synchronouse">
+<node CREATED="1143036760990" ID="Freemind_Link_1667661764" MODIFIED="1143036763093" TEXT="CORBA"/>
+</node>
+<node CREATED="1143036755272" ID="Freemind_Link_1440620375" MODIFIED="1143036759909" TEXT="asynchronouse"/>
+</node>
+<node CREATED="1143036789892" ID="Freemind_Link_1995527353" MODIFIED="1143036797453" TEXT="Meaning of Adapters"/>
+</node>
+<node CREATED="1143037002708" ID="Freemind_Link_1850129948" MODIFIED="1143037007355" TEXT="Instrumentation">
+<node CREATED="1143037016828" ID="Freemind_Link_257507708" MODIFIED="1143037027333" TEXT="preprocess of application to generate events"/>
+<node CREATED="1143037028795" ID="Freemind_Link_858675786" MODIFIED="1143037031870" TEXT="assertion in java"/>
+</node>
+<node CREATED="1143037066910" ID="Freemind_Link_471848198" MODIFIED="1143037077045" TEXT="CEP as Source"/>
+<node CREATED="1143040140232" ID="Freemind_Link_964369243" MODIFIED="1143040253585" TEXT="Layered Enterprise Systems&#xa;(How do they fit in a system)">
+<node CREATED="1143040149135" ID="Freemind_Link_980137981" MODIFIED="1143040156946" TEXT="Corporate Level">
+<node CREATED="1143040157708" ID="Freemind_Link_1901960060" MODIFIED="1143040160191" TEXT="Strategic"/>
+<node CREATED="1143040161393" ID="Freemind_Link_1414288815" MODIFIED="1143040168273" TEXT="Business"/>
+</node>
+<node CREATED="1143040171978" ID="Freemind_Link_274988639" MODIFIED="1143040184526" TEXT="collaboration layer"/>
+<node CREATED="1143040186910" ID="Freemind_Link_1634856983" MODIFIED="1143040192888" TEXT="middleware layer"/>
+<node CREATED="1143040194540" ID="Freemind_Link_523985265" MODIFIED="1143040201510" TEXT="network layer"/>
+</node>
+</node>
+<node CREATED="1143036512413" ID="Freemind_Link_1825306676" MODIFIED="1143036517891" TEXT="CEP Definition"/>
+<node CREATED="1143037165562" ID="Freemind_Link_944840043" MODIFIED="1143037168056" TEXT="Relationships">
+<node CREATED="1143037168807" ID="Freemind_Link_1548995706" MODIFIED="1143037170149" TEXT="Time">
+<node CREATED="1143037176478" ID="Freemind_Link_416005483" MODIFIED="1143037194914" TEXT="dimension of problems regarding timezones">
+<node CREATED="1143037171130" ID="Freemind_Link_2257188" MODIFIED="1143037172913" TEXT="ax"/>
+<node CREATED="1143037174154" ID="Freemind_Link_1109531021" MODIFIED="1143037175186" TEXT="tx"/>
+</node>
+<node CREATED="1143037372690" ID="Freemind_Link_409409019" MODIFIED="1143037427759" TEXT="What can do it for me?">
+<node CREATED="1143037429011" ID="Freemind_Link_1799857095" MODIFIED="1143037461928" TEXT="can define relationship between events through their temporal ordering"/>
+<node CREATED="1143037463140" ID="Freemind_Link_1889987724" MODIFIED="1143037476549" TEXT="performance measuring"/>
+<node CREATED="1143037478752" ID="Freemind_Link_648350484" MODIFIED="1143037576263" TEXT="debug filter"/>
+</node>
+</node>
+<node CREATED="1143037275420" ID="Freemind_Link_101030857" MODIFIED="1143037277703" TEXT="Cause">
+<node CREATED="1143037577304" ID="Freemind_Link_719053413" MODIFIED="1143037599216" TEXT="transitive, asymetric and partial ordered"/>
+<node CREATED="1143037601068" ID="Freemind_Link_1162792067" MODIFIED="1143037615199" TEXT="direct acyclic graphs for viz"/>
+<node CREATED="1143039467883" ID="Freemind_Link_1616956370" MODIFIED="1143039474863" TEXT="Horizontal Causality"/>
+<node CREATED="1143039478358" ID="Freemind_Link_1134375653" MODIFIED="1143039481572" TEXT="Vertical Causality"/>
+</node>
+<node CREATED="1143037288779" ID="Freemind_Link_1539632421" MODIFIED="1143037293867" TEXT="Cause Time Axiom"/>
+<node CREATED="1143037278825" ID="Freemind_Link_1284018423" MODIFIED="1143037284303" TEXT="Aggregation"/>
+</node>
+</node>
 <node CREATED="1141426342000" ID="Freemind_Link_580159673" MODIFIED="1141426352796" TEXT="bezug &quot;Power of Events&quot;"/>
+<node CREATED="1141928855411" ID="Freemind_Link_1234323669" MODIFIED="1141928862952" TEXT="Warum will ich Events suchen">
+<node CREATED="1141928878774" ID="Freemind_Link_1498090436" MODIFIED="1141928884903" TEXT="Szenarios"/>
+</node>
+<node CREATED="1141928918441" ID="Freemind_Link_1548270823" MODIFIED="1141928938330" TEXT="Abgrenzung dieser Arbeit neben dem gro&#xdf;en Bild"/>
+<node CREATED="1140098155046" ID="Freemind_Link_841435694" MODIFIED="1141426474484" TEXT="Anwendungsgebiete von Eventbasierten Systemen">
+<node CREATED="1143040631809" ID="Freemind_Link_46222021" MODIFIED="1143040638279" TEXT="Security"/>
+<node CREATED="1143040641463" ID="Freemind_Link_1180671015" MODIFIED="1143040642635" TEXT="BI"/>
+<node CREATED="1143040643847" ID="Freemind_Link_693370377" MODIFIED="1143040644598" TEXT="CRM"/>
+<node CREATED="1143040645819" ID="Freemind_Link_351440472" MODIFIED="1143040653260" TEXT="Supply Chain"/>
+<node CREATED="1143040654432" ID="Freemind_Link_1009569333" MODIFIED="1143040655353" TEXT="ERP"/>
+<node CREATED="1143040657566" ID="Freemind_Link_1183981295" MODIFIED="1143040680399" TEXT="anything"/>
+</node>
 </node>
 <node CREATED="1140098111750" ID="Freemind_Link_1957745656" MODIFIED="1140098140796" TEXT="Bedeutung der Auffindung von Daten">
 <node CREATED="1140098336718" ID="Freemind_Link_1345148021" MODIFIED="1140098366796" TEXT="Probleme"/>
@@ -23,7 +203,6 @@
 <node CREATED="1140098248109" ID="Freemind_Link_513671466" MODIFIED="1140098257562" TEXT="Das Zeug was die in Stanford machen"/>
 <node CREATED="1140098425562" ID="Freemind_Link_1944109159" MODIFIED="1140098432000" TEXT="... &lt;recherchieren&gt;"/>
 </node>
-<node CREATED="1140098155046" ID="Freemind_Link_841435694" MODIFIED="1141426474484" TEXT="Anwendungsgebiete von Eventbasierten Systemen"/>
 </node>
 <node CREATED="1140098221734" ID="Freemind_Link_978760588" MODIFIED="1140098575265" POSITION="left" TEXT="Event Google">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
